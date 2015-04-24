233f537b60bc0f0983d8ec8b6fac76b4b0c9912b
Kunal Krishna <kunalkrishna85@gmail.com>
2015-04-24 16:20:16 +0530

    Update clean.bat
    comment updated

--- clean.bat
+++ clean.bat
@@ -2,6 +2,5 @@ del /S *.o *.exe
 
 ::pause
 
-:: ^ remove the :: in above line if you want to see logs
+::  remove the " :: " in above ^ line if you want to see logs
 
-:: ^ prepend :: before pause in the above line if you dont want to see the logs
